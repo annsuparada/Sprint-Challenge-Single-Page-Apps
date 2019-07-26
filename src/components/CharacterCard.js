4ba2cdb56aaec24a2fadf6f431e522e045540ca2
Suparada Saitalae <ann.suparada@gmail.com>
2019-07-26 11:30:15 -0500

    maping charactors

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard (props) {
+  const { name} = props
+  return (
+    <div>
+      {/* <image className="" src={img} alt={name}/> */}
+      <h2>{name}</h2>
+      {/* <p>{species}</p>
+      <p>{status}</p>
+      <h3>Location: {location}</h3>
+      <h3>Origin: {origin}</h3> */}
+      
+
+    </div>
+  )
 }
